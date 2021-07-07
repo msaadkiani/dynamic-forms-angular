d4a6fa74f9d5f22b8d071eed1e7f99ca91076fd4
msaadkiani <saadi.iqbal@gmail.com>
2021-07-07 10:54:06 +0500

    Angular adding forms dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'forms';
+export class AppComponent 
+{
+    nameLog: any;
+    theForm: FormGroup;
+    formVal: any;
+    constructor(private fb: FormBuilder) 
+    {
+        this.theForm = new FormGroup({});
+    }
+
+    ngOnInit(): void 
+    {
+        this.theForm = this.fb.group({
+            name: ['', [
+                Validators.required
+            ]],
+
+            phone: this.fb.array([])
+        })
+
+        this.addPhone();
+        console.log(this.theForm.controls.name, this.theForm.controls.phone)
+    }
+
+    formValue()
+    {
+        this.formVal = this.theForm.value;
+        for (let i = 0; i < this.Phone.length; i++) 
+        {
+            console.log(this.Phone.at(i).value);    
+        }
+        console.log(this.theForm.value, this.theForm.controls.name.value, this.theForm.controls);
+    }
+
+    get Phone()
+    {
+        return this.theForm.get('phone') as FormArray;
+    }
+
+    get name()
+    {
+        return this.theForm.get('name');
+    }
+
+    addPhone()
+    {
+        const phone = (this.theForm.get('phone') as FormArray); 
+        phone.push(this.fb.group({
+            phone: ['']
+        }))
+
+        console.log(phone);
+    }
+
+    deletePhone(i: any)
+    {
+        if ( this.Phone.length > 1)
+        this.Phone.removeAt(i);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
